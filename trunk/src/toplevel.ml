10966fb2c7fb839a921d4141d5ecd267a4d2d0f5
stone <stone>
2004-02-13 04:22:29 +0000

    Added usage message to toplevel.ml

--- trunk/src/toplevel.ml
+++ trunk/src/toplevel.ml
@@ -1,6 +1,7 @@
 open Message
 
 let read fn =
+  let _ = print_string ("parsing " ^ fn ^ "\n") in
   let fin = open_in fn in
   let e = Parser.theoryspecs Lexer.token (Lexing.from_channel fin) in
     close_in fin ;
@@ -8,4 +9,7 @@ let read fn =
 
 let parse str = Parser.theoryspecs Lexer.token (Lexing.from_string str);;
 
-read Sys.argv.(1);;
+if Array.length(Sys.argv) = 2 then 
+  ignore (read Sys.argv.(1))
+else
+  print_string ("Usage:  " ^ Sys.argv.(0) ^ " <filename to parse>\n");;
