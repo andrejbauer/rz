cd52e44ce48e0d23f7c4cb228d0fa997fe7d3e95
andrej <andrej>
2005-02-06 12:06:50 +0000

    moved .thr files to .thy files

--- trunk/examples/nat.thy
+++ trunk/examples/nat.thy
@@ -1,12 +1,17 @@
 # natural numbers
 
+theory Iteration =
+thy
+  set a
+  const x : a
+  const s : a -> a
+end
+
 theory Nat =
 thy
   set nat
   const zero : nat
   const succ : nat -> nat
-
-  implicit n : nat
 
   # the existential in the following axiom should really
   # be unique existential, which we don't have right now
@@ -24,20 +29,5 @@ thy
       f = f')
     )
 
-  # The principle of induction shuld turn out to be equivalent to the
-  # initiality axiom.
 
-
-  axiom induction1 [T : thy predicate p : nat end] =
-    (T.p zero and all n . (T.p n => T.p (succ n))) => all n . T.p n
-
-
-
-  axiom induction2 [T : thy
-                         predicate p : nat
-
-                         axiom p0 = p zero
-                         axiom p1 n = p (n) => p (succ n)
-                       end] =
-    all n . (T.p n)
 end
