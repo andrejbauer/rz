48d8788375e21b46b4d7372718622cf9eea5da66
andrej <andrej>
2005-02-10 23:19:34 +0000

    *** empty log message ***

---
+++ trunk/doc/case/semigroup.thy
@@ -0,0 +1,13 @@
+theory T =
+thy
+  set s
+  const e : s
+  const ( * ) : s -> s -> s
+  implicit x, y, z : s
+  axiom unit x =
+    x * e = x and e * x = x
+  axiom assoc x y z =
+    x * (y * z) = (x * y) * z
+  axiom sqrt x =
+    some y . y * y = x
+end
