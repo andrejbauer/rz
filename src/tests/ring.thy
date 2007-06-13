85202bfe7412420c434bb1f0b1d439e7e0dab544
stone <stone>
2007-06-13 13:46:14 +0000

    Added ring.thy test for renaming of theory elements

---
+++ src/tests/ring.thy
@@ -0,0 +1,25 @@
+Definition Monoid := thy
+  Parameter s : Set.
+  Parameter one : s.
+  Parameter mul : s -> s -> s.
+
+  Axiom associative : forall x y z : s, mul x (mul y z) = mul (mul x y) z.
+  (* Plus other axioms *)
+end.
+
+Definition Group := thy
+  include Monoid.
+
+  Parameter inv : s -> s.
+  
+  Axiom rightinverse : forall x : s, mul x (inv x) = one.
+  (* Plus other axioms *)
+end.
+
+Definition Ring := thy
+  include Monoid. 
+  include Group with mul -> add 
+                with one -> zero.
+end.
+
+  
